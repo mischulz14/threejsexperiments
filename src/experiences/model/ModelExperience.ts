0776b4a1c7a32e56a100adeb8ca82912a42bd5f0
mischulz14 <mi.schulz@hotmail.com>
2026-02-18 00:55:44 +0100

    add new model and hdr map

--- src/experiences/model/ModelExperience.ts
+++ src/experiences/model/ModelExperience.ts
@@ -1,9 +1,21 @@
-import { Mesh, PointLight } from 'three';
+import {
+  AmbientLight,
+  DirectionalLight,
+  EquirectangularReflectionMapping,
+  Mesh,
+  PointLight,
+  SpotLight,
+  Vector3,
+} from 'three';
 import {
   DRACOLoader,
   GLTFLoader,
+  HDRLoader,
   KTX2Loader,
+  RGBELoader,
 } from 'three/examples/jsm/Addons.js';
+import PMREMGenerator from 'three/src/renderers/common/extras/PMREMGenerator.js';
+import type { Renderer } from 'three/webgpu';
 
 import type { Experience } from '../../Experience';
 import type { IExperience } from '../../types/types';
@@ -24,20 +36,35 @@ export class ModelExperience implements IExperience {
     const ktxLoader = new KTX2Loader();
     gltfLoader.setDRACOLoader(dracoLoader).setKTX2Loader(ktxLoader);
 
-    gltfLoader.load('/models/roadScene_compressed.glb', (scene) => {
-      scene.scene.scale.set(0.3, 0.3, 0.3);
-      scene.scene.position.set(0, 0, 0);
-      experience.camera?.position.set(0, 2, -4);
-      experience.add(scene.scene);
-
-      this.addLights(experience);
+    const loader = new HDRLoader();
+    const envMap = loader.load('/hdr/abandoned_greenhouse_1k.hdr', () => {
+      envMap.mapping = EquirectangularReflectionMapping;
+      experience.environment = envMap;
+      experience.renderer?.setClearColor(0x000000);
     });
+
+    // --- LOAD MODEL ---
+    gltfLoader.load(
+      '/models/ornament_hietzinger_metallic_ultra_compressed.glb',
+      (gltf) => {
+        gltf.scene.position.set(0, 0, 0);
+
+        experience.camera?.position.set(0, 0, 10);
+        experience.camera?.lookAt(0, 0, 0);
+
+        experience.add(gltf.scene);
+
+        this.addLights(experience);
+      },
+    );
   }
 
   addLights(experience: Experience) {
-    const pointLight = new PointLight('0xfff', 10);
-    pointLight.position.set(0, 4, 0);
-    experience.add(pointLight);
+    const spotLight = new SpotLight(0xffffff, 100, 0, Math.PI / 2);
+
+    spotLight.position.set(0, 3, 0);
+    experience.add(spotLight);
+    experience.add(new AmbientLight(0xffffff, 3));
   }
 
   step() {}
